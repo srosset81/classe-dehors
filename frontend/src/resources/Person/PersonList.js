7b3ce9ddbb49be25888c8ea91d44370b3120f25a
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-05-07 21:39:25 +0200

    remove Export button from "Annuaire"

--- frontend/src/resources/Person/PersonList.js
+++ frontend/src/resources/Person/PersonList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { List, SimpleList } from '@semapps/archipelago-layout';
 import PersonFilterSidebar from "./PersonFilterSidebar";
+import { ListActions } from "react-admin";
 import { Avatar } from "@material-ui/core";
 import PersonIcon from '@material-ui/icons/Person';
 
 const PersonList = props => (
-  <List title="Annuaire" aside={<PersonFilterSidebar />} sort={{ field: 'pair:label', order: 'DESC' }} perPage={100} pagination={false} {...props}>
+  <List
+    title="Annuaire"
+    aside={<PersonFilterSidebar />}
+    sort={{ field: 'pair:label', order: 'DESC' }}
+    perPage={100}
+    pagination={false}
+    actions={<ListActions exporter={false}/>}
+    {...props}
+  >
     <SimpleList
       primaryText={record => record['pair:label']}
       leftAvatar={() => (
