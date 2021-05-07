974df5a95192231b658b84759433ed7912248b04
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-05-08 01:19:27 +0200

    add introduction (editable in markdown) for place and person

--- frontend/src/resources/Person/PersonList.js
+++ frontend/src/resources/Person/PersonList.js
@@ -4,27 +4,32 @@ import PersonFilterSidebar from "./PersonFilterSidebar";
 import { ListActions } from "react-admin";
 import { Avatar } from "@material-ui/core";
 import PersonIcon from '@material-ui/icons/Person';
+import MarkdownIntroduction from "../../markdown/MarkdownIntroduction";
 
-const PersonList = props => (
-  <List
-    title="Annuaire"
-    aside={<PersonFilterSidebar />}
-    sort={{ field: 'pair:label', order: 'DESC' }}
-    perPage={100}
-    pagination={false}
-    actions={<ListActions exporter={false}/>}
-    {...props}
-  >
-    <SimpleList
-      primaryText={record => record['pair:label']}
-      leftAvatar={() => (
-        <Avatar width="100%">
-          <PersonIcon />
-        </Avatar>
-      )}
-      linkType="show"
-    />
-  </List>
+const PersonList = (props) => (
+  <>
+    <MarkdownIntroduction pageId="annuaire-introduction" />
+    <List
+      title=""
+      aside={<PersonFilterSidebar />}
+      sort={{ field: "pair:label", order: "DESC" }}
+      perPage={100}
+      pagination={false}
+      actions={<ListActions exporter={false}/>}
+      {...props}
+    >
+        <SimpleList
+          primaryText={(record) => record["pair:label"]}
+          leftAvatar={() => (
+            <Avatar width="100%">
+              <PersonIcon />
+            </Avatar>
+          )}
+          linkType="show"
+        />
+     
+    </List>
+  </>
 );
 
 export default PersonList;
