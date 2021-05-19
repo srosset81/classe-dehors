80071a4b16c5b72304a9406a2c6e35c8c34ac669
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-05-19 20:06:50 +0200

    use date-fns to display date of events

--- frontend/src/resources/Event/EventList.js
+++ frontend/src/resources/Event/EventList.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { DateField, ReferenceField, TextField } from 'react-admin';
+import { ReferenceField, TextField } from 'react-admin';
 import { Avatar } from '@material-ui/core';
 import { List, SimpleList, ListActions } from '@semapps/archipelago-layout';
 import EventIcon from '@material-ui/icons/Event';
 import EventFilterSidebar from './EventFilterSidebar';
+import isSameDay from 'date-fns/isSameDay';
+import format from 'date-fns/format';
 
 const EventList = props => (
   <List
@@ -16,50 +18,57 @@ const EventList = props => (
     <SimpleList
       primaryText={record => record["pair:label"]}
       secondaryText={record => (
-          <ReferenceField
-            record={record}
-            reference="Organization"
-            basePath="Organization" // Hack to get access to to Organization data
-            source="pair:operatedBy"
-            linkType="show"
-          >
-            <TextField source="pair:label" />
-          </ReferenceField>
-        )
-      }
-      tertiaryText={(record) => (
-        <>
-          Du&nbsp;
-          <DateField
-            record={record}
-            source="pair:startDate"
-            options={{
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-              hour: "2-digit",
-              minute: "2-digit",
-            }}
-          />
-          &nbsp;au&nbsp;
-          <DateField
-            record={record}
-            source="pair:endDate"
-            options={{
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-              hour: "2-digit",
-              minute: "2-digit",
-            }}
-          />
-        </>
+        <ReferenceField
+          record={record}
+          reference="Organization"
+          basePath="Organization" // Hack to get access to to Organization data
+          source="pair:operatedBy"
+          linkType="show"
+        >
+          <TextField source="pair:label" />
+        </ReferenceField>
       )}
-      leftAvatar={() =>
+      tertiaryText={record => {
+        if (record["pair:startDate"] && record["pair:endDate"]) {
+          if (
+            isSameDay(
+              new Date(record["pair:startDate"]),
+              new Date(record["pair:endDate"])
+            )
+          ) {
+            // Same day : Le xx/xx/xxxx de tt:tt à tt:tt
+            return `Le\xa0${format(
+              new Date(record["pair:startDate"]),
+              "dd/MM/yyyy"
+            )} de\xa0${format(
+              new Date(record["pair:startDate"]),
+              "kk:mm"
+            )}\xa0à\xa0${format(new Date(record["pair:endDate"]), "kk:mm")}`;
+          } else {
+            // Different days : Du xx/xx/xxxx tt:tt au xx/xx/xxxx tt:tt
+            return `Du\xa0${format(
+              new Date(record["pair:startDate"]),
+              "dd/MM/yyyy\xa0kk:mm"
+            )} au\xa0${format(
+              new Date(record["pair:endDate"]),
+              "dd/MM/yyyy\xa0kk:mm"
+            )}`;
+          }
+        }
+        if (record["pair:startDate"]) {
+          // Only start day indicated : La xx/xx/xxxx à tt:tt
+          return `Le\xa0${format(
+            new Date(record["pair:startDate"]),
+            "dd/MM/yyyy"
+          )}\xa0à\xa0${format(new Date(record["pair:startDate"]), "kk:mm")}`;
+        }
+        return "";
+      }}
+      leftAvatar={() => (
         <Avatar width="100%">
           <EventIcon />
         </Avatar>
-      }
+      )}
       linkType="show"
     />
   </List>
