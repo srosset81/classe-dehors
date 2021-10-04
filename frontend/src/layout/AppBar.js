9f485a4d4231d8db28957999ab88ba7715b1f3b6
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-04 11:51:32 +0200

    add submenu

--- frontend/src/layout/AppBar.js
+++ frontend/src/layout/AppBar.js
@@ -1,61 +1,150 @@
-import React from 'react';
-import { Container, Box, Grid, makeStyles, Typography, AppBar as MuiAppBar, useMediaQuery, IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Container,
+  Box,
+  Grid,
+  makeStyles,
+  Typography,
+  AppBar as MuiAppBar,
+  useMediaQuery,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   header: {
-    position: 'relative',
+    position: "relative",
     padding: 10,
     height: 65,
-    [theme.breakpoints.down('sm')]: {
-      height: 50
-    }
+    [theme.breakpoints.down("sm")]: {
+      height: 50,
+    },
   },
   logo: {
     width: 48,
     height: 48,
-    verticalAlign: 'middle',
-    [theme.breakpoints.down('sm')]: {
+    verticalAlign: "middle",
+    [theme.breakpoints.down("sm")]: {
       width: 32,
       height: 32,
-    }
+    },
   },
   logoArea: {
     padding: 12,
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down("xs")]: {
       padding: 5,
-    }
+    },
   },
   logoText: {
-    fontFamily: 'Helvetica',
+    fontFamily: "Helvetica",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: theme.palette.common.white,
     // verticalAlign: 'middle',
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       fontSize: 18,
-    }
+    },
   },
   openButton: {
     padding: 5,
-    float: 'right'
+    float: "right",
   },
   menuLink: {
-    textDecoration: 'none'
+    textDecoration: "none",
   },
   menuText: {
-    fontFamily: 'Helvetica',
+    fontFamily: "Helvetica",
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
     lineHeight: 1,
-    color: theme.palette.common.white
-  }
+    color: theme.palette.common.white,
+  },
+  subMenu: {
+    cursor: "pointer",
+  },
 }));
+
+const InternalMenuLink = ({ text, link, classes }) => (
+  <Grid item sm={2} key={text}>
+    <Box display="flex" height={48} alignItems="center" justifyContent="center">
+      <Link to={link} className={classes.menuLink}>
+        <Typography className={classes.menuText}>
+          {text.split("\n").map((item, key) => (
+            <React.Fragment key={key}>
+              {item}
+              <br />
+            </React.Fragment>
+          ))}
+        </Typography>
+      </Link>
+    </Box>
+  </Grid>
+);
+
+const ExternalMenuLink = ({ text, link, classes }) => (
+  <Grid item sm={2} key={text}>
+    <Box display="flex" height={48} alignItems="center" justifyContent="center">
+      <a href={link} target="_blank" className={classes.menuLink}>
+        <Typography className={classes.menuText}>
+          {text.split("\n").map((item, key) => (
+            <React.Fragment key={key}>
+              {item}
+              <br />
+            </React.Fragment>
+          ))}
+        </Typography>
+      </a>
+    </Box>
+  </Grid>
+);
+
+const MenuSubLinks = ({ children, handleMenuClose, classes }) => {
+  return children.map(({ text, internal, link }) => (
+    <MenuItem onClick={handleMenuClose}>
+      {internal ? (
+        <Link to={link} className={classes.menuLink}>
+          <Typography>
+            {text.split("\n").map((item, key) => (
+              <React.Fragment key={key}>
+                {item}
+                <br />
+              </React.Fragment>
+            ))}
+          </Typography>
+        </Link>
+      ) : (
+        <a href={link} target="_blank" className={classes.menuLink}>
+          <Typography>
+            {text.split("\n").map((item, key) => (
+              <React.Fragment key={key}>
+                {item}
+                <br />
+              </React.Fragment>
+            ))}
+          </Typography>
+        </a>
+      )}
+    </MenuItem>
+  ));
+};
 
 const AppBar = ({ menuItems, setSidebarOpen }) => {
   const classes = useStyles();
-  const xs = useMediaQuery(theme => theme.breakpoints.down('xs'));
+  const xs = useMediaQuery((theme) => theme.breakpoints.down("xs"));
+
+  const [anchorMenuEl, setMenuAnchorEl] = React.useState(null);
+  const open = Boolean(anchorMenuEl);
+  const handleMenuClick = (event) => {
+    console.log(event.currentTarget);
+    setMenuAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setMenuAnchorEl(null);
+  };
+
   return (
     <MuiAppBar position="sticky">
       <Container maxWidth="lg" className={classes.header}>
@@ -76,24 +165,58 @@ const AppBar = ({ menuItems, setSidebarOpen }) => {
                 <MenuIcon />
               </IconButton>
             ) : (
-            <Grid container>
-              {Object.keys(menuItems).map(link => (
-                <Grid item sm={2} key={link}>
-                  <Box display="flex" height={48} alignItems="center" justifyContent="center">
-                    <Link to={link} className={classes.menuLink}>
-                      <Typography className={classes.menuText}>
-                        {menuItems[link].split('\n').map((item, key) => (
-                          <React.Fragment key={key}>
-                            {item}
-                            <br />
-                          </React.Fragment>
-                        ))}
-                      </Typography>
-                    </Link>
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
+              <Grid container>
+                {menuItems.map(({ text, internal, link, children }) => {
+                  if (link) {
+                    if (internal) {
+                      return <InternalMenuLink {...{ text, link, classes }} />;
+                    } else {
+                      return <ExternalMenuLink {...{ text, link, classes }} />;
+                    }
+                  } else {
+                    return (
+                      <>
+                        <Grid item sm={2} key={text}>
+                          <Box
+                            display="flex"
+                            height={48}
+                            alignItems="center"
+                            justifyContent="center"
+                            className={classes.subMenu}
+                            onClick={handleMenuClick}
+                          >
+                            <Typography className={classes.menuText}>
+                              {text.split("\n").map((item, key) => (
+                                <React.Fragment key={key}>
+                                  {item}
+                                  <br />
+                                </React.Fragment>
+                              ))}
+                            </Typography>
+                          </Box>
+                        </Grid>
+                        <Menu
+                          anchorEl={anchorMenuEl}
+                          open={open}
+                          onClose={handleMenuClose}
+                          anchorOrigin={{
+                            vertical: "bottom",
+                            horizontal: "left",
+                          }}
+                          transformOrigin={{
+                            vertical: "top",
+                            horizontal: "left",
+                          }}
+                        >
+                          <MenuSubLinks
+                            {...{ children, handleMenuClose, classes }}
+                          />
+                        </Menu>
+                      </>
+                    );
+                  }
+                })}
+              </Grid>
             )}
           </Grid>
         </Grid>
--- frontend/src/layout/SideMenu.js
+++ frontend/src/layout/SideMenu.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Drawer, makeStyles } from "@material-ui/core";
+import { Drawer, Divider, makeStyles } from "@material-ui/core";
 import { MenuItemLink } from "react-admin";
 
 const useStyles = makeStyles(() => ({
   paper: {
     paddingTop: 10,
-    minWidth: 200
-  }
+    minWidth: 200,
+  },
 }));
 
 const SideMenu = ({ menuItems, sidebarOpen, setSidebarOpen }) => {
@@ -18,15 +18,46 @@ const SideMenu = ({ menuItems, sidebarOpen, setSidebarOpen }) => {
       classes={{ paper: classes.paper }}
       onClose={() => setSidebarOpen(false)}
     >
-      {Object.keys(menuItems).map(link => (
-        <MenuItemLink
-          key={link}
-          to={link}
-          primaryText={menuItems[link]}
-        />
-      ))}
+      {menuItems.map(({ text, internal, link, children }) => {
+        if (link) {
+          if (internal) {
+            return <MenuItemLink key={link} to={link} primaryText={text} />;
+          } else {
+            return (
+              <MenuItemLink
+                primaryText={text}
+                key={link}
+                to={{ pathname: link }}
+                target="_blank"
+              />
+            );
+          }
+        } else {
+          return (
+            <>
+              <Divider />
+              {children.map(({ text, internal, link }) => {
+                if (internal) {
+                  return (
+                    <MenuItemLink key={link} to={link} primaryText={text} />
+                  );
+                } else {
+                  return (
+                    <MenuItemLink
+                      primaryText={text}
+                      key={link}
+                      to={{ pathname: link }}
+                      target="_blank"
+                    />
+                  );
+                }
+              })}
+            </>
+          );
+        }
+      })}
     </Drawer>
   );
-}
+};
 
 export default SideMenu;
