021ad1b3ce48cd132272a916b72b8090f30ced8e
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-14 20:19:11 +0200

    remove all actions from tha map

--- frontend/src/resources/Place/PlaceList.js
+++ frontend/src/resources/Place/PlaceList.js
@@ -1,62 +1,36 @@
 import React from "react";
-import { MultiViewsList, SimpleList } from "@semapps/archipelago-layout";
+
 import { MapList } from "@semapps/geo-components";
-import { Avatar } from "@material-ui/core";
-import MapIcon from "@material-ui/icons/Map";
-import ListIcon from "@material-ui/icons/List";
-import PlaceIcon from "@material-ui/icons/Place";
+
 import MarkdownIntroduction from "../../markdown/MarkdownIntroduction";
+import { List } from "react-admin";
 
 const PlaceList = (props) => (
   <>
     <MarkdownIntroduction pageId="place-introduction" />
-    <MultiViewsList
-      views={{
-        map: {
-          label: "Carte",
-          icon: MapIcon,
-          perPage: 500,
-          pagination: false,
-          list: (
-            <MapList
-              latitude={(record) =>
-                record?.["pair:hasPostalAddress"]?.["pair:latitude"]
-              }
-              longitude={(record) =>
-                record?.["pair:hasPostalAddress"]?.["pair:longitude"]
-              }
-              label={(record) => record?.["pair:label"]}
-              description={(record) =>
-                record?.["pair:hasPostalAddress"]?.["pair:label"]
-              }
-              scrollWheelZoom
-            />
-          ),
-        },
-        list: {
-          label: "Liste",
-          icon: ListIcon,
-          sort: { field: "pair:label", order: "DESC" },
-          perPage: 25,
-          list: (
-            <SimpleList
-              primaryText={(record) => record["pair:label"]}
-              secondaryText={(record) =>
-                record["pair:hasPostalAddress"]?.["pair:label"]
-              }
-              leftAvatar={() => (
-                <Avatar width="100%">
-                  <PlaceIcon />
-                </Avatar>
-              )}
-              linkType="show"
-            />
-          ),
-        },
-      }}
-      exporter={false}
+
+    <List
       {...props}
-    />
+      perPage={1000}
+      pagination={false}
+      label="Carte"
+      exporter={false}
+      actions={false}
+    >
+      <MapList
+        latitude={(record) =>
+          record?.["pair:hasPostalAddress"]?.["pair:latitude"]
+        }
+        longitude={(record) =>
+          record?.["pair:hasPostalAddress"]?.["pair:longitude"]
+        }
+        label={(record) => record?.["pair:label"]}
+        description={(record) =>
+          record?.["pair:hasPostalAddress"]?.["pair:label"]
+        }
+        scrollWheelZoom
+      />
+    </List>
   </>
 );
 
