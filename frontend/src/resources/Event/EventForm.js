3486a86e60acab2a83ea9752e93c2440535b8735
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-14 21:13:36 +0200

    add introductive text to calendar

--- frontend/src/resources/Event/EventForm.js
+++ frontend/src/resources/Event/EventForm.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { required, SimpleForm, TextInput } from 'react-admin';
-import MarkdownInput from '../../markdown/MarkdownInput';
-import { DateTimeInput } from '@semapps/date-components';
+import React from "react";
+import { required, SimpleForm, TextInput } from "react-admin";
+import MarkdownInput from "../../markdown/MarkdownInput";
+import { DateTimeInput } from "@semapps/date-components";
 import frLocale from "date-fns/locale/fr";
 import ReferenceQuickCreateInput from "../../pair/ReferenceQuickCreateInput";
 
 const urlValidation = (value) => {
-  if (value && !value.startsWith('http://') && !value.startsWith('https://')) {
-    return 'Veuillez entrer une adresse valide';
+  if (value && !value.startsWith("http://") && !value.startsWith("https://")) {
+    return "Veuillez entrer une adresse valide";
   }
   return undefined;
 };
 
-const EventForm = props => (
+const EventForm = (props) => (
   <SimpleForm {...props} redirect="show">
     <TextInput source="pair:label" validate={required()} fullWidth />
     <ReferenceQuickCreateInput
@@ -43,26 +43,30 @@ const EventForm = props => (
     <DateTimeInput
       source="pair:startDate"
       options={{
-        format: 'dd/MM/yyyy à HH:mm',
-        ampm: false
+        format: "dd/MM/yyyy à HH:mm",
+        ampm: false,
       }}
       providerOptions={{
-        locale: frLocale
+        locale: frLocale,
       }}
       fullWidth
     />
     <DateTimeInput
       source="pair:endDate"
       options={{
-        format: 'dd/MM/yyyy à HH:mm',
-        ampm: false
+        format: "dd/MM/yyyy à HH:mm",
+        ampm: false,
       }}
       providerOptions={{
-        locale: frLocale
+        locale: frLocale,
       }}
       fullWidth
     />
-    <TextInput source="cd:registrationPage" validate={urlValidation} fullWidth />
+    <TextInput
+      source="cd:registrationPage"
+      validate={urlValidation}
+      fullWidth
+    />
     <TextInput source="cd:meetingPage" validate={urlValidation} fullWidth />
   </SimpleForm>
 );
--- frontend/src/resources/Event/EventList.js
+++ frontend/src/resources/Event/EventList.js
@@ -1,77 +1,91 @@
-import React from 'react';
-import { ReferenceField, TextField } from 'react-admin';
-import { Avatar } from '@material-ui/core';
-import { List, SimpleList, ListActions } from '@semapps/archipelago-layout';
-import EventIcon from '@material-ui/icons/Event';
-import EventFilterSidebar from './EventFilterSidebar';
-import isSameDay from 'date-fns/isSameDay';
-import format from 'date-fns/format';
+import React from "react";
+import { ReferenceField, TextField } from "react-admin";
+import { Avatar } from "@material-ui/core";
+import { List, SimpleList } from "@semapps/archipelago-layout";
+import EventIcon from "@material-ui/icons/Event";
 
-const EventList = props => (
-  <List
-    title="Calendrier"
-    aside={<EventFilterSidebar />}
-    sort={{ field: "pair:startDate", order: "DESC" }}
-    actions={<ListActions exporter={false} />}
-    {...props}
-  >
-    <SimpleList
-      primaryText={record => record["pair:label"]}
-      secondaryText={record => (
-        <ReferenceField
-          record={record}
-          reference="Organization"
-          basePath="Organization" // Hack to get access to to Organization data
-          source="pair:operatedBy"
-          linkType="show"
-        >
-          <TextField source="pair:label" />
-        </ReferenceField>
-      )}
-      tertiaryText={record => {
-        if (record["pair:startDate"] && record["pair:endDate"]) {
-          if (
-            isSameDay(
-              new Date(record["pair:startDate"]),
-              new Date(record["pair:endDate"])
-            )
-          ) {
-            // Same day : Le xx/xx/xxxx de tt:tt à tt:tt
+import isSameDay from "date-fns/isSameDay";
+import format from "date-fns/format";
+
+import React from "react";
+
+import { Avatar } from "@material-ui/core";
+import { List, SimpleList } from "@semapps/archipelago-layout";
+import EventIcon from "@material-ui/icons/Event";
+import MarkdownIntroduction from "../../markdown/MarkdownIntroduction";
+
+const EventList = (props) => (
+  <>
+    <MarkdownIntroduction pageId="agenda-introduction" />
+    <List
+      {...props}
+      title="Calendrier"
+      sort={{ field: "pair:startDate", order: "DESC" }}
+      actions={false}
+    >
+      <SimpleList
+        primaryText={(record) => record["pair:label"]}
+        secondaryText={(record) => {
+          if (record["pair:startDate"] && record["pair:endDate"]) {
+            if (
+              isSameDay(
+                new Date(record["pair:startDate"]),
+                new Date(record["pair:endDate"])
+              )
+            ) {
+              // Same day : Le xx/xx/xxxx de tt:tt à tt:tt
+              return `Le\xa0${format(
+                new Date(record["pair:startDate"]),
+                "dd/MM/yyyy"
+              )} de\xa0${format(
+                new Date(record["pair:startDate"]),
+                "kk:mm"
+              )}\xa0à\xa0${format(new Date(record["pair:endDate"]), "kk:mm")}`;
+            } else {
+              // Different days : Du xx/xx/xxxx tt:tt au xx/xx/xxxx tt:tt
+              return `Du\xa0${format(
+                new Date(record["pair:startDate"]),
+                "dd/MM/yyyy\xa0kk:mm"
+              )} au\xa0${format(
+                new Date(record["pair:endDate"]),
+                "dd/MM/yyyy\xa0kk:mm"
+              )}`;
+            }
+          }
+          if (record["pair:startDate"]) {
+            // Only start day indicated : La xx/xx/xxxx à tt:tt
             return `Le\xa0${format(
               new Date(record["pair:startDate"]),
               "dd/MM/yyyy"
-            )} de\xa0${format(
-              new Date(record["pair:startDate"]),
-              "kk:mm"
-            )}\xa0à\xa0${format(new Date(record["pair:endDate"]), "kk:mm")}`;
-          } else {
-            // Different days : Du xx/xx/xxxx tt:tt au xx/xx/xxxx tt:tt
-            return `Du\xa0${format(
-              new Date(record["pair:startDate"]),
-              "dd/MM/yyyy\xa0kk:mm"
-            )} au\xa0${format(
-              new Date(record["pair:endDate"]),
-              "dd/MM/yyyy\xa0kk:mm"
-            )}`;
+            )}\xa0à\xa0${format(new Date(record["pair:startDate"]), "kk:mm")}`;
           }
-        }
-        if (record["pair:startDate"]) {
-          // Only start day indicated : La xx/xx/xxxx à tt:tt
-          return `Le\xa0${format(
-            new Date(record["pair:startDate"]),
-            "dd/MM/yyyy"
-          )}\xa0à\xa0${format(new Date(record["pair:startDate"]), "kk:mm")}`;
-        }
-        return "";
-      }}
-      leftAvatar={() => (
-        <Avatar width="100%">
-          <EventIcon />
-        </Avatar>
-      )}
-      linkType="show"
-    />
-  </List>
+          return "";
+        }}
+        tertiaryText={(record) => {
+          if (record["pair:operatedBy"]) {
+            return (
+              <ReferenceField
+                record={record}
+                reference="Organization"
+                basePath="Organization" // Hack to get access to to Organization data
+                source="pair:operatedBy"
+                linkType="show"
+              >
+                <TextField source="pair:label" />
+              </ReferenceField>
+            );
+          }
+          return "";
+        }}
+        leftAvatar={() => (
+          <Avatar width="100%">
+            <EventIcon />
+          </Avatar>
+        )}
+        linkType="show"
+      />
+    </List>
+  </>
 );
 
 export default EventList;
