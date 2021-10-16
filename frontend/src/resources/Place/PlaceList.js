b5f8dc2f6de64e574c1a86afc8459b57d2b79971
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-16 08:30:30 +0200

    show user on the map

--- frontend/src/resources/Place/PlaceList.js
+++ frontend/src/resources/Place/PlaceList.js
@@ -3,7 +3,62 @@ import React from "react";
 import { MapList } from "@semapps/geo-components";
 
 import MarkdownIntroduction from "../../markdown/MarkdownIntroduction";
-import { List } from "react-admin";
+
+import {
+  List,
+  ShowButton,
+  EditButton,
+  useResourceDefinition,
+  ReferenceArrayField,
+  SimpleList,
+} from "react-admin";
+import PersonIcon from "@material-ui/icons/Person";
+import { Avatar, Typography } from "@material-ui/core";
+
+const PopupContent = ({ record, basePath }) => {
+  const resourceDefinition = useResourceDefinition({});
+  return (
+    <>
+      {record.label && <Typography variant="h5">{record.label}</Typography>}
+      {record.description && (
+        <Typography>
+          {record.description.length > 150
+            ? record.description.substring(0, 150) + "..."
+            : record.description}
+        </Typography>
+      )}
+      {record["pair:locationOf"] && (
+        <ReferenceArrayField
+          reference="Person"
+          source="pair:locationOf"
+          record={{
+            ...record,
+            "pair:locationOf":
+              typeof record["pair:locationOf"] === "string"
+                ? [record["pair:locationOf"]]
+                : record["pair:locationOf"],
+          }}
+        >
+          <SimpleList
+            primaryText={(record) => record?.["pair:label"]}
+            leftAvatar={() => (
+              <Avatar width="100%">
+                <PersonIcon />
+              </Avatar>
+            )}
+            linkType="show"
+          />
+        </ReferenceArrayField>
+      )}
+      {resourceDefinition.hasShow && (
+        <ShowButton basePath={basePath} record={record} />
+      )}
+      {resourceDefinition.hasEdit && (
+        <EditButton basePath={basePath} record={record} />
+      )}
+    </>
+  );
+};
 
 const PlaceList = (props) => (
   <>
@@ -29,6 +84,7 @@ const PlaceList = (props) => (
           record?.["pair:hasPostalAddress"]?.["pair:label"]
         }
         scrollWheelZoom
+        popupContent={PopupContent}
       />
     </List>
   </>
