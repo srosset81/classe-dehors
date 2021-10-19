2643a4df1faf2994df352c607949e8fef82f4d70
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-19 09:26:52 +0200

    show user type

--- frontend/src/resources/Person/PersonList.js
+++ frontend/src/resources/Person/PersonList.js
@@ -4,6 +4,7 @@ import PersonFilterSidebar from "./PersonFilterSidebar";
 import { Avatar } from "@material-ui/core";
 import PersonIcon from "@material-ui/icons/Person";
 import MarkdownIntroduction from "../../markdown/MarkdownIntroduction";
+import { ReferenceField, TextField } from "react-admin";
 
 const displayLocationLabel = (record) => {
   if (!record["pair:hasLocation"]) {
@@ -33,6 +34,22 @@ const PersonList = (props) => (
       <SimpleList
         primaryText={(record) => record["pair:label"]}
         secondaryText={displayLocationLabel}
+        tertiaryText={(record) => {
+          if (record["pair:hasType"]) {
+            return (
+              <ReferenceField
+                record={record}
+                reference="Type"
+                basePath="Type" // Hack to get access to to Organization data
+                source="pair:hasType"
+                link={false}
+              >
+                <TextField source="pair:label" />
+              </ReferenceField>
+            );
+          }
+          return "";
+        }}
         leftAvatar={() => (
           <Avatar width="100%">
             <PersonIcon />
